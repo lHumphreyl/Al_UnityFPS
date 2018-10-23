63a5280174a649daf8f682bb2d724dca40fd1368
IHumphreyI <humprey.omega@gmail.com>
2018-10-24 02:18:42 +0300

    Добавлен фонарик. Добавлена площадь с темной постройкой для теста фонарика.

---
+++ Al_FPS/Assets/_Scripts/Controllers/FlashlightController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS
+{
+	public class FlashlightController : BaseController 
+	{
+		private FlashlightModel _model;
+
+		private void Awake()
+		{
+			_model = FindObjectOfType<FlashlightModel> ();
+			Off ();
+		}
+
+		public override void On()
+		{
+			base.On ();
+			_model.On ();
+		}
+
+		public override void Off()
+		{
+			base.Off ();
+			_model.Off ();
+		}
+
+		public void Switch()
+		{
+			if (IsEnabled)
+				Off ();
+			else
+				On ();
+		}
+	}
+}
